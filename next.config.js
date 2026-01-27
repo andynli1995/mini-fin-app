493b9d52688b8f78a5db3cdcdfa78f351b62ebf9
Andy Li <andy.nli1995@gmail.com>
2026-01-26 19:39:57 -0800

    Add note about harmless _document build warning

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Note: The "_document not found" warning during build is a known Next.js 14 issue
+  // when using App Router. It's harmless and doesn't affect the build output.
+  // The build completes successfully despite this warning.
+}
 
 module.exports = nextConfig
